ce881bfb218beae39b2c4fe9d5473f9415d1d19d
anonOstrich <jesper.kuutti@helsinki.fi>
2019-02-05 09:27:01 +0200

    Mahdollisuus blogin luontiin, ->5.3]

--- src/App.js
+++ src/App.js
@@ -3,6 +3,41 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+
+//noteform as a component of its own
+const NoteForm = ({updateShownBlogs}) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+
+  const createBlog = async event => {
+    event.preventDefault()
+    try{
+      const result = await blogService.create({title, author, url})
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+      updateShownBlogs(result)
+    } catch(exception){
+      alert('jotain meni blogin luonnissa vikaan :(')
+    }
+
+  }
+
+  return (
+    <div>
+      <h2>Create new</h2>
+      <form onSubmit={createBlog}>
+        title <input value={title} name='Title' onChange={event => {setTitle(event.target.value)}}/><br />
+        author <input value={author} name='Value' onChange={event => {setAuthor(event.target.value)}}/><br />
+        url <input value={url} name='Url' onChange={event => {setUrl(event.target.value)}}/><br />
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
@@ -19,7 +54,9 @@ const App = () => {
   useEffect(() => {
     const jsonUser = window.localStorage.getItem('loggedAppUser')
     if(jsonUser){
-      setUser(JSON.parse(jsonUser))
+      const loggedUser = JSON.parse(jsonUser)
+      setUser(loggedUser)
+      blogService.setToken(loggedUser.token)
     }
   }, [])
 
@@ -33,6 +70,7 @@ const App = () => {
 
       setUser(createdUser);
       window.localStorage.setItem('loggedAppUser', JSON.stringify(createdUser))
+      blogService.setToken(createdUser.token)
       setUsername('')
       setPassword('')
     } catch(exception) {
@@ -46,13 +84,14 @@ const App = () => {
   }
 
 
+
   if(user === null){
     return(
     <div>
       <h2>Log in to application</h2>
       <form onSubmit={login}>
-        käyttäjätunnus <input type="text" name='Username' value={username} onChange={event => {setUsername(event.target.value)}}/> <br />
-        salasana <input type="password" name='Password' value={password} onChange={event => {setPassword(event.target.value)}}/>
+        käyttäjätunnus <input type="text"  value={username} onChange={event => {setUsername(event.target.value)}}/> <br />
+        salasana <input type="password"  value={password} onChange={event => {setPassword(event.target.value)}}/>
         <button type="submit">kirjaudu</button>
       </form>
     </div>)
@@ -65,6 +104,9 @@ const App = () => {
         <p>{user.name} logged in</p>
         <button onClick={logout}>logout</button>
       </div>
+      <NoteForm  updateShownBlogs={
+        newBlog => {setBlogs(blogs.concat(newBlog))}
+      }/>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -2,9 +2,23 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`
+}
+
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-export default { getAll }
+const create = async (blog) => {
+
+  console.log("token:", token)
+  const result = await axios.post(baseUrl, blog, {
+    headers: {Authorization: token}
+  })
+  return result.data
+}
+export default { getAll, setToken, create }
