3c7976cdecd6b681e4c648b9b8fb318e2ef4c95c
anonOstrich <jesper.kuutti@helsinki.fi>
2019-02-05 08:40:55 +0200

    Ensimmäinen kirjautumistoiminnallisuus; tehty ->5.1]

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
+
+  const login = async (event) => {
+    event.preventDefault()
+    try{
+
+    const createdUser = await loginService.sendLoginInfo(
+      username, password
+    )
+
+    console.log(createdUser)
+      setUser(createdUser);
+      setUsername('')
+     setPassword('')
+    } catch(exception) {
+      alert('login failed!')
+    }
+  }
+
+
+  if(user === null){
+    return(
+    <div>
+      <h2>Log in to application</h2>
+      <form onSubmit={login}>
+        käyttäjätunnus <input type="text" name='Username' value={username} onChange={event => {setUsername(event.target.value)}}/> <br />
+        salasana <input type="password" name='Password' value={password} onChange={event => {setPassword(event.target.value)}}/>
+        <button type="submit">kirjaudu</button>
+      </form>
+    </div>)
+  }
 
   return (
     <div>
