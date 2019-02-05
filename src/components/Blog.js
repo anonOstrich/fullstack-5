830f40d4e092970a9a668c853363abd1a373270a
anonOstrich <jesper.kuutti@helsinki.fi>
2019-02-05 15:27:39 +0200

    Tehty -> 5.6]

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,36 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, {useState} from 'react'
+
+const Blog = ({ blog }) =>{
+
+  const [verbose, setVerbose] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showOnVerbose = { display: verbose ? '' : 'none'}
+
+  const toggleVerbosity = () => {
+    setVerbose(!verbose)
+  }
+
+  return (
+  <div style={blogStyle}>
+    <div onClick={toggleVerbosity}>
+      {blog.title} {blog.author}
+      <div>
+      <div style={showOnVerbose}>
+      <a href={blog.url}>{blog.url}</a><br/>
+      {blog.likes} likes <button>like</button> <br/>
+      added by {blog.hasOwnProperty('user') ? blog.user.name : 'unknown'} <br/>
+      </div>
+    </div>
+    </div>
   </div>
-)
+)}
 
 export default Blog
