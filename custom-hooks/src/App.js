0b69763afbcf853bbdbbbf291f4e4baf5d079ba9
Jesper Kuutti <jesper@Jesper-Air.home>
2019-02-11 23:08:02 +0200

    Tehty custom hooks loppuun

--- custom-hooks/src/App.js
+++ custom-hooks/src/App.js
@@ -19,17 +19,24 @@ const useField = (type) => {
 const useResource =  (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  const getAll =  () => {
-    axios.get(baseUrl).then(response => {
-      setResources(response.data)
-     return response.data
-    })
+  const setInitial = async () => {
+    const data = await getAll()
+    setResources(data)
   }
 
-  const create = (resource) => {
-    axios.post(baseUrl, resource).then(response => {
-      setResources(resources.concat(response.data))
-      return response.data})
+  useEffect(() => {
+    setInitial()
+  }, [])
+
+  const getAll = async () => {
+    const response = await axios.get(baseUrl)
+     return response.data
+  }
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+    return response.data
   }
 
   const service = {
@@ -52,15 +59,14 @@ const App = () => {
 
 
 
-
-  const handleNoteSubmit = (event) => {
+  const handleNoteSubmit = async (event) => {
     event.preventDefault()
-    noteService.create({ content: content.value })
+    await noteService.create({ content: content.value })
   }
- 
-  const handlePersonSubmit = (event) => {
+
+  const handlePersonSubmit = async (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    await personService.create({ name: name.value, number: number.value})
   }
 
   return (
@@ -70,14 +76,14 @@ const App = () => {
         <input {...content} />
         <button>Create</button>
       </form>
-      { notes.map(n => <p key={n.id}>{n.content}</p>) }
+      {notes &&  notes.map(n => <p key={n.id}>{n.content}</p>) }
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
         name <input {...name} /> <br/>
         number <input {...number} />
         <button>Create</button>
       </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+      {persons && persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
     </div>
   )
 }
